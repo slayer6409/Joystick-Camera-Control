6e7ce7339aea2a5b3104d27bba3af912767337a6
slayer6409 <andrews.webs6409@gmail.com>
2021-01-27 02:43:42 -0600

    Changes based off Janni9009's suggestions.

--- Joystick Camera Control/JoystickCameraControl.cs
+++ Joystick Camera Control/JoystickCameraControl.cs
@@ -6,14 +6,14 @@ using VRC.UserCamera;
 using MelonLoader;
 
 
-[assembly: MelonInfo(typeof(JoystickCameraControl), "Joystick Camera Control", "1.0", "Slayer6409")]
+[assembly: MelonInfo(typeof(JoystickCameraControl), "Joystick Camera Control", "1.1", "Slayer6409")]
 [assembly: MelonGame("VRChat", "VRChat")]
 
 
 namespace Joystick_Camera_Control
 {
 
-    //Thanks to nitrog0d and loukylor for help with this
+    //Thanks to nitrog0d, loukylor, and Janni9009 for help with this
     //My code is probably a bit weird, but oh well it works.
 
     public class JoystickCameraControl : MelonMod
@@ -25,61 +25,54 @@ namespace Joystick_Camera_Control
             ICustomShowableLayoutedMenu cm = ExpansionKitApi.CreateCustomQuickMenuPage(LayoutDescription.QuickMenu3Columns);
             ExpansionKitApi.GetExpandedMenu(ExpandedMenu.QuickMenu).AddSimpleButton("Toggle Camera Movement",toggleCameraMovement);
         }
-
+        
         public override void OnUpdate()
         {
-            player = VRCPlayer.field_Internal_Static_VRCPlayer_0;
-            if (player == null) return;
-            //-1 to 1
-            
-
             if (CameraMovement)
             {
                 if (Input.GetKeyDown(KeyCode.Joystick1Button9))
                 {
-                    if (turnMode) turnMode = false;
-                    else turnMode = true;
+                    turnMode = !turnMode;
                 }
-
                 if (turnMode)
                 {
-                    float movementMod = 2.0f;
-                    if (Input.GetAxis("Horizontal") > .2f) rotate(Input.GetAxis("Horizontal") * movementMod);
-                    if (Input.GetAxis("Horizontal") < -.2f) rotate(Input.GetAxis("Horizontal") * movementMod);
-                    if (Input.GetAxis("Vertical") < -.2f) rotateVert(Input.GetAxis("Vertical") * movementMod);
-                    if (Input.GetAxis("Vertical") > .2f) rotateVert(Input.GetAxis("Vertical") * movementMod);
+                    var turnMovementMod = 1.5f;
+                    if (Input.GetAxis("Horizontal") > .2f) rotate(Input.GetAxis("Horizontal") * turnMovementMod);
+                    if (Input.GetAxis("Horizontal") < -.2f) rotate(Input.GetAxis("Horizontal") * turnMovementMod);
+                    if (Input.GetAxis("Vertical") < -.2f) rotateVert(Input.GetAxis("Vertical") * turnMovementMod);
+                    if (Input.GetAxis("Vertical") > .2f) rotateVert(Input.GetAxis("Vertical") * turnMovementMod);
                 }
                 else
                 {
-                    float movementMod = 15;
-                    if (Input.GetAxis("Horizontal") > .2f) moveHorizontal(Input.GetAxis("Horizontal") / movementMod);
-                    if (Input.GetAxis("Horizontal") < -.2f) moveHorizontal(Input.GetAxis("Horizontal") / movementMod);
-                    if (Input.GetAxis("Vertical") < -.2f) moveBack(Input.GetAxis("Vertical") / movementMod);
-                    if (Input.GetAxis("Vertical") > .2f) moveForward(Input.GetAxis("Vertical") / movementMod);
+                    if (Input.GetAxis("Horizontal") > .2f) moveHorizontal(Input.GetAxis("Horizontal"));
+                    if (Input.GetAxis("Horizontal") < -.2f) moveHorizontal(Input.GetAxis("Horizontal"));
+                    if (Input.GetAxis("Vertical") < -.2f) moveFrontBack(Input.GetAxis("Vertical"));
+                    if (Input.GetAxis("Vertical") > .2f) moveFrontBack(Input.GetAxis("Vertical"));
                 }
-                //moveHorizontal(float speed)
-                //if (Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickHorizontal") > .2f) rotateVert(Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickHorizontal")*1.5f);
-                //if (Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickHorizontal") < -.2f) rotateVert(Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickHorizontal")*1.5f);
-                if (Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical") < -.2f) moveUp(Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical") / 12);
-                if (Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical") > .2f) moveDown(Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical") / 12);
+                //if (Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickHorizontal") > .2f)
+                //if (Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickHorizontal") < -.2f) 
+                if (Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical") < -.2f) moveY(Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical"));
+                if (Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical") > .2f) moveY(Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical"));
             }
         }
         
 
         private void toggleCameraMovement()
         {
+            player = VRCPlayer.field_Internal_Static_VRCPlayer_0;
+            if (player == null) return;
             if (CameraMovement)
             {
                 
                 CameraMovement = false;
-                player.field_Private_VRCPlayerApi_0.SetWalkSpeed(4);
-                player.field_Private_VRCPlayerApi_0.SetRunSpeed(8);
-                player.field_Private_VRCPlayerApi_0.SetStrafeSpeed(4);
+                player.field_Private_VRCPlayerApi_0.SetWalkSpeed(walkSpeed);
+                player.field_Private_VRCPlayerApi_0.SetRunSpeed(runSpeed);
+                player.field_Private_VRCPlayerApi_0.SetStrafeSpeed(strafeSpeed);
 
             }
             else
             {
-                var cam = getCam();
+                var cam = UserCameraController.field_Internal_Static_UserCameraController_0;
                 if (cam != null)
                 {
                     while (cam.prop_EnumPublicSealedvaAtLoWoCO5vUnique_0 != EnumPublicSealedvaAtLoWoCO5vUnique.World)
@@ -87,6 +80,9 @@ namespace Joystick_Camera_Control
                         cam.viewFinder.transform.Find("PhotoControls/Left_Space").GetComponent<CameraInteractable>().Interact();
                     }
                 }
+                walkSpeed = player.field_Private_VRCPlayerApi_0.GetWalkSpeed();
+                runSpeed = player.field_Private_VRCPlayerApi_0.GetRunSpeed();
+                strafeSpeed = player.field_Private_VRCPlayerApi_0.GetStrafeSpeed();
                 CameraMovement = true;
                 player.field_Private_VRCPlayerApi_0.SetWalkSpeed(0);
                 player.field_Private_VRCPlayerApi_0.SetRunSpeed(0);
@@ -94,75 +90,58 @@ namespace Joystick_Camera_Control
             }
         }
 
-        private void moveBack(float speed)
+
+
+        private void moveFrontBack(float speed)
         {
-            var camController = getCam();
+            var camController = UserCameraController.field_Internal_Static_UserCameraController_0;
             var camRot = worldCameraQuaternion.ToEuler();
             if (camController == null) return;
-            worldCameraVector += new Vector3((float)Math.Sin(camRot.y) * speed, 0f, (float)Math.Cos(camRot.y) * speed);
-
-        }
-
-        private void moveForward(float speed)
-        {
-            var camController = getCam();
-            var camRot = worldCameraQuaternion.ToEuler();
-            if (camController == null) return;
-            worldCameraVector += new Vector3((float)Math.Sin(camRot.y) * speed, 0f, (float)Math.Cos(camRot.y) * speed);
+            worldCameraVector += new Vector3((float)Math.Sin(camRot.y) * (speed * Time.deltaTime), 0f, (float)Math.Cos(camRot.y) * (speed * Time.deltaTime));
 
         }
         private void moveHorizontal(float speed)
         {
-            var camController = getCam();
+            var camController = UserCameraController.field_Internal_Static_UserCameraController_0;
             var camRot = worldCameraQuaternion.ToEuler();
             if (camController == null) return;
-            worldCameraVector += Vector3.Cross(Vector3.up,new Vector3((float)Math.Sin(camRot.y), 0f, (float)Math.Cos(camRot.y))*speed);
+            worldCameraVector += Vector3.Cross(Vector3.up,new Vector3((float)Math.Sin(camRot.y), 0f, (float)Math.Cos(camRot.y))*(speed*Time.deltaTime));
 
         }
 
-        private void moveUp(float speed)
+        private void moveY(float speed)
         {
-            var camController = getCam();
+            var camController = UserCameraController.field_Internal_Static_UserCameraController_0;
             if (camController == null) return;
-            worldCameraVector += new Vector3(0f, speed, 0f);
-
-        }
-        private void moveDown(float speed)
-        {
-            var camController = getCam();
-            if (camController == null) return;
-            worldCameraVector += new Vector3(0f, speed, 0f);
+            worldCameraVector += new Vector3(0f, speed * Time.deltaTime, 0f);
 
         }
 
 
         public void rotate(float dir)
         {
-            var camController = getCam();
+            var camController = UserCameraController.field_Internal_Static_UserCameraController_0;
             if (camController == null) return;
             worldCameraQuaternion *= Quaternion.Euler(0f, dir, 0f);
         }
         public void rotateVert(float dir)
         {
-            var camController = getCam();
+            var camController = UserCameraController.field_Internal_Static_UserCameraController_0;
             if (camController == null) return;
             worldCameraQuaternion *= Quaternion.Euler(dir, 0f, 0f);
         }
 
-        public static UserCameraController getCam()
-        {
-            return UserCameraController.field_Internal_Static_UserCameraController_0;
-        }
+        
 
         public static Vector3 worldCameraVector
         {
             get
             {
-                return getCam().field_Private_Vector3_0;
+                return UserCameraController.field_Internal_Static_UserCameraController_0.field_Private_Vector3_0;
             }
             set
             {
-                getCam().field_Private_Vector3_0 = value;
+                UserCameraController.field_Internal_Static_UserCameraController_0.field_Private_Vector3_0 = value;
             }
         }
 
@@ -170,18 +149,21 @@ namespace Joystick_Camera_Control
         {
             get
             {
-                return getCam().field_Private_Quaternion_0;
+                return UserCameraController.field_Internal_Static_UserCameraController_0.field_Private_Quaternion_0;
             }
             set
             {
-                getCam().field_Private_Quaternion_0 = value;
+                UserCameraController.field_Internal_Static_UserCameraController_0.field_Private_Quaternion_0 = value;
             }
         }
 
         public static bool CameraMovement { get; set; } = false;
         public static bool turnMode { get; set; } = false;
         public static VRCPlayer player { get; set; }
-        public static float mSpeed { get; set; } = 0.25f;
+        public static float walkSpeed { get; set; }
+        public static float runSpeed { get; set; }
+        public static float strafeSpeed { get; set; }
+        public static float mSpeed { get; set; } = 0.15f;
 
     }
 }
